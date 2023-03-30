eee1de77268b5c3b28ccaf1a3ae879762f974454
Samuel Sharp <samuelsharp4@gmail.com>
2023-03-30 13:16:41 -0700

    Mobile compatibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,15 +18,17 @@ export default function Home() {
                 </div>
             </div>
             <div className='mobileHero'>
-                <h1 style={{ fontSize: '1.6rem', color: 'black' }}>Go beyond <Typewriter
+                <video src="/hero.mp4" loop autoPlay muted className='heroImageMobile' />
+                <h1 className='mobileHeroContent' style={{ fontSize: '1.6rem', color: 'black' }}>Go beyond <Typewriter
                     options={{
                         strings: ['Barrio Logan', 'Chula Vista', 'East Village', 'Hillcrest', 'Little Italy', 'North Park', 'Old Town', 'Pacific Beach', 'Point Loma', 'Scripps Ranch', 'City Heights', 'Tierrasanta', 'Kearney Mesa', 'Clairemont'],
                         autoStart: true,
                         loop: true,
-                        wrapperClassName: 'heroContentTypewriter',
+                        wrapperClassName: 'heroContentTypewriterMobile',
                         cursorClassName: 'heroContentTypewriterCursor'
                     }}
-                />and explore San Diego.</h1>
+                />and explore San Diego.
+                </h1>
             </div>
             <div className='pageContent'>
                 {/* <h1 className='pageBigHeader'>You've got the card. Now, it's time to go with PRONTO.</h1> */}
